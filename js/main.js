bdbfc29c098fdcce0828339d6444a917e35a10d1
Hyejin Kim <khyejin08@naver.com>
2025-06-16 13:13:24 +0900

    header nav 반응형 기능 구현

--- js/main.js
+++ js/main.js
@@ -1,15 +1,39 @@
 // header 스크롤 애니메이션
-$(window).on("scroll", function () {
-  let scrollTop = $(this).scrollTop();
+function scrollHeader() {
+  if (window.matchMedia("screen and (min-width: 1024px)").matches) {
+    let scrollTop = $(this).scrollTop();
 
-  if (scrollTop >= 80) {
-    $("header").addClass("active");
-  } else if (scrollTop < 80) {
-    $("header").removeClass("active");
+    if (scrollTop >= 80) {
+      $(".header").addClass("active");
+    } else if (scrollTop < 80) {
+      $(".header").removeClass("active");
+    }
   }
+}
+
+$(window).on("scroll", function () {
+  scrollHeader();
 });
 
-// intro text-wrap 애니메이션
-$(window).on("load", function () {
-  $(".intro-text-wrap").addClass("load");
+$(".open-mobile-nav").on("click", function () {
+  $(".header").addClass("mobile-active");
+  $(".nav").addClass("active");
+  $(".mobile-logo").addClass("active");
+});
+
+$(".close-mobile-nav").on("click", function () {
+  $(".header").removeClass("mobile-active");
+  $(".nav").removeClass("active");
+  $(".mobile-logo").removeClass("active");
+});
+
+$(document).on("click", function (e) {
+  if (
+    $(".nav").has(e.target).length === 0 &&
+    $(e.target).closest(".open-mobile-nav").length === 0
+  ) {
+    $(".header").removeClass("mobile-active");
+    $(".nav").removeClass("active");
+    $(".mobile-logo").removeClass("active");
+  }
 });
