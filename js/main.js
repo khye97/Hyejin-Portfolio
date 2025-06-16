eff2484d45a526dbf8b3dcbf3f2df240ca70b11e
Hyejin Kim <khyejin08@naver.com>
2025-06-16 14:28:08 +0900

    intro 섹션 load 기능 구현

--- js/main.js
+++ js/main.js
@@ -15,18 +15,21 @@ $(window).on("scroll", function () {
   scrollHeader();
 });
 
+// nav 열기
 $(".open-mobile-nav").on("click", function () {
   $(".header").addClass("mobile-active");
   $(".nav").addClass("active");
   $(".mobile-logo").addClass("active");
 });
 
+// nav 닫기
 $(".close-mobile-nav").on("click", function () {
   $(".header").removeClass("mobile-active");
   $(".nav").removeClass("active");
   $(".mobile-logo").removeClass("active");
 });
 
+// nav 외부 클릭시 nav 닫기
 $(document).on("click", function (e) {
   if (
     $(".nav").has(e.target).length === 0 &&
@@ -36,4 +39,9 @@ $(document).on("click", function (e) {
     $(".nav").removeClass("active");
     $(".mobile-logo").removeClass("active");
   }
+});
+
+// intro text-wrap 애니메이션
+$(window).on("load", function () {
+  $(".intro-text-wrap").addClass("load");
 });
